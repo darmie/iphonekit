14bd7344cb533ff33c3397788a2b286379f20c49
Damilare Darmie Akinlaja <dreakinlaja@gmail.com>
2017-12-04 11:10:11 +0100

    move installations/builds protocol to vagrant provision

--- system/provision.sh
+++ system/provision.sh
@@ -7,26 +7,25 @@ apt-get autoremove -y
 # Create logs directory
 su - vagrant -c "mkdir /vagrant/system/logs"
 
-cd /vagrant
-sudo git clone https://github.com/libimobiledevice/libimobiledevice.git
+
 sudo apt-get install -y libtool
 sudo apt-get install -y m4 automake
-sudo apt-get install -y  libplist++-dev libplist++3v5 libplist-dbg libplist-dev libplist-doc libplist-utils  libplist3  python-plist
+sudo apt-get install -y pkg-config libplist++-dev libplist++3v5 libplist-dbg libplist-dev libplist-doc libplist-utils  libplist3  python-plist
 sudo apt-get install -y python-dev libssl-dev libzip-dev
 
-cd /libusbmuxd
-./autogen.sh
-make
-sudo make install
+sudo git clone https://github.com/libimobiledevice/libusbmuxd.git
+cd libusbmuxd
+./autogen.sh && make && sudo make install
 
-cd ..
-cd /libimobiledevice
-./autogen.sh
-make
-sudo make install
+cd /home/ubuntu
+sudo git clone https://github.com/libimobiledevice/libimobiledevice.git
+cd libimobiledevice
+./autogen.sh && make && sudo make install
 
-cd ..
-cd /ideviceinstaller
-./autogen.sh
-make 
-sudo make install
+cd /home/ubuntu
+sudo git clone https://github.com/darmie/ideviceinstaller.git
+cd ideviceinstaller
+./autogen.sh && make && sudo make install
+
+sudo ldconfig
+ideviceinstaller --help
